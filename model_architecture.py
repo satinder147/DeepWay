c5747e75cabfe62b2247d5c042dca9edbab6bda6
satinder singh <satindersingh_16277@aitpune.edu.in>
2018-06-27 23:36:03 +0530

    Update model_architecture.py

--- model_architecture.py
+++ model_architecture.py
@@ -33,51 +33,6 @@ class Model:
         return model
 
 
-    def model2(self):
-        model=Sequential()
-        input_Shape=(self.height,self.width,self.channels)
-        if(k.image_data_format=="channels_first"):
-            input_Shape=(self.channels,self.height,self.width)
-        model.add(Conv2D(20,(5,5),padding="same",input_shape=input_Shape))
-        model.add(Activation("relu"))
-        model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)))
-        model.add(Conv2D(50,(5,5),padding="same"))
-        model.add(Activation("relu"))
-        model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)))
-        model.add(Conv2D(100,(5,5),padding="same"))
-        model.add(Activation("relu"))
-        model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)))
-        model.add(Flatten())
-        model.add(Dense(500))
-        model.add(Activation("relu"))
-        model.add(Dense(self.num_classes))
-        model.add(Activation("softmax"))
-        return model
-
-
-
-
-    def model3(self):
-        model=Sequential()
-        input_Shape=(self.height,self.width,self.channels)
-        if(k.image_data_format=="channels_first"):
-            input_Shape=(self.channels,self.height,self.width)
-        model.add(Conv2D(20,(5,5),padding="same",input_shape=input_Shape,kernel_regularizer=regularizers.l2(0.01)))
-        model.add(LeakyReLU(alpha=0.1))
-        model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)))
-        model.add(Conv2D(50,(5,5),padding="same",kernel_regularizer=regularizers.l2(0.01)))
-        model.add(LeakyReLU(alpha=0.1))
-        model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)))
-        model.add(Conv2D(100,(5,5),padding="same",kernel_regularizer=regularizers.l2(0.01)))
-        model.add(LeakyReLU(alpha=0.1))
-        model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)))
-        model.add(Flatten())
-        model.add(Dense(500))
-        model.add(LeakyReLU(alpha=0.1))
-        model.add(Dense(self.num_classes))
-        model.add(Activation("softmax"))
-        return model
-
 
 
 
