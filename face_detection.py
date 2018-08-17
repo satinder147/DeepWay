b03a711a0c32421e6635ac09a2e276d64f646735
kranthi99 <gkranthi.kiran.99@gmail.com>
2018-08-17 20:16:46 +0530

    Refactored

--- face_detection.py
+++ face_detection.py
@@ -1,25 +1,26 @@
 import dlib
 import cv2
 
+
 class detect:
     def __init__(self):
-        self.detector=dlib.get_frontal_face_detector()
-    def draw(self,frame,x,y,w,h):
-        frame=cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-        return frame,(x+w)
+        self.detector = dlib.get_frontal_face_detector()
+
+    def draw(self, frame, x, y, w, h):
+        frame = cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        return frame, (x+w)
         
-    def faceDetect(self,frame):
-        y=0
+    def faceDetect(self, frame):
+        y = 0
         
-        frame=cv2.resize(frame,(480,640))
-        grey=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        rect=self.detector(grey,1)
-        for i,d in enumerate(rect):
-            frame,y=self.draw(frame,d.left(),d.top(),d.right()-d.left(),d.bottom()-d.top())
-        if(y==0):
-            return frame,-1
-        elif(y<150):
-            return frame,1
-        elif(y>=150):
-            return frame,0
-        
+        frame = cv2.resize(frame, (480, 640))
+        grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        rect = self.detector(grey, 1)
+        for i, d in enumerate(rect):
+            frame, y = self.draw(frame, d.left(), d.top(), d.right()-d.left(), d.bottom()-d.top())
+        if y == 0:
+            return frame, -1
+        elif y < 150:
+            return frame, 1
+        elif y >= 150:
+            return frame, 0
